6b10d83ba320f5192a536691356853d18eb81284
Chintan Mistry <48353167+mistrychintan@users.noreply.github.com>
2025-02-27 14:12:53 -0500

    Add files via upload

---
+++ CREATE_DATABASE.sql
@@ -0,0 +1,155 @@
+--CREATE A DATABASE FOR BIKE RENTAL SHOP
+
+CREATE DATABASE BIKE_RENTAL_SHOP;
+
+--USE BIKE_RENTAL_SHOP DATABASE
+
+USE BIKE_RENTAL_SHOP;
+
+--CREATE A CUSTOMER TABLE
+
+CREATE TABLE customer
+	(
+		ID INT NOT NULL,
+		[Name] VARCHAR(30),
+		Email VARCHAR(30),
+		PRIMARY KEY (ID)
+	);
+
+--CREATE A TABLE FOR BIKE
+
+CREATE TABLE bike 
+	(
+		ID INT NOT NULL,
+		model VARCHAR(50),
+		category VARCHAR(50),
+		price_per_hour DECIMAL,
+		price_per_day DECIMAL,
+		[status] VARCHAR(20),
+		PRIMARY KEY (ID)
+	);
+
+--CREATE A RENTAL TABLE
+DROP TABLE IF exists rental;
+CREATE TABLE rental  
+	(
+		ID INT NOT NULL PRIMARY KEY,
+		customer_id INT FOREIGN KEY REFERENCES customer(ID),
+		bike_id INT FOREIGN KEY REFERENCES bike(ID),
+		start_timestamp DATETIME,
+		duration INT,
+		total_paid DECIMAL
+	);
+
+--CREATE A MEMBERSHIP_TYPE TABLE
+DROP TABLE IF exists membership_type;
+
+CREATE TABLE membership_type
+	(
+		ID INT NOT NULL PRIMARY KEY,
+		[name] VARCHAR(50),
+		[description] VARCHAR(100),
+		price DECIMAL
+	);
+
+--CREATE A MEMBERSHIP TABLE
+
+CREATE TABLE membership
+	(
+		ID INT NOT NULL PRIMARY KEY,
+		membership_type_id INT FOREIGN KEY REFERENCES membership_type(ID),
+		customer_id INT	FOREIGN KEY REFERENCES customer(ID),
+		[start_date] DATE,
+		end_date DATE,
+		total_paid DECIMAL
+	);
+
+--INSERT DATA INTO CUSTOMER TABLE
+
+insert into customer values(1,	'John Doe',			'john.doe@example.com');
+insert into customer values(2,	'Alice Smith',		'alice.smith@example.com');
+insert into customer values(3,	'Bob Johnson',		'bob.johnson@example.com');
+insert into customer values(4,	'Eva Brown',		'eva.brown@example.com');
+insert into customer values(5,	'Michael Lee',		'michael.lee@example.com');
+insert into customer values(6,	'Sarah White',		'sarah.white@example.com');
+insert into customer values(7,	'David Wilson',		'david.wilson@example.com');
+insert into customer values(8,	'Emily Davis',		'emily.davis@example.com');
+insert into customer values(9,	'Daniel Miller',	'daniel.miller@example.com');
+insert into customer values(10,	'Olivia Taylor',	'olivia.taylor@example.com');
+
+--INSERT DATA INTO BIKE TABLE
+
+insert into bike values(1,	'Mountain Bike 1'	,'mountain bike'	,10.00	,50.00	,'available');
+insert into bike values(2,	'Road Bike 1'	,'road bike',12.00	,60.00	,'available');
+insert into bike values(3,	'Hybrid Bike 1'	,'hybrid'	,8.00	,40.00	,'rented');
+insert into bike values(4,	'Electric Bike 1'	,'electric',15.00	,75.00	,'available');
+insert into bike values(5,	'Mountain Bike 2'	,'mountain bike'	,10.00	,50.00	,'out of service');
+insert into bike values(6,	'Road Bike 2'	,'road bike',12.00	,60.00	,'available');
+insert into bike values(7,	'Hybrid Bike 2'	,'hybrid'	,8.00	,40.00	,'out of service');
+insert into bike values(8,	'Electric Bike 2'	,'electric',15.00	,75.00	,'available');
+insert into bike values(9,	'Mountain Bike 3'	,'mountain bike'	,10.00	,50.00	,'rented');
+insert into bike values(10,	'Road Bike 3'	,'road bike',12.00	,60.00	,'available');
+
+--INSERT DATA INTO A RENTAL TABLE
+
+insert into rental	values(1,	1,	1,	'2022-11-01 10:00:00',	240,	50.00);
+insert into rental values(2,	1,	1,	'2022-11-02 10:00:00',	245,	50.00);
+insert into rental values(3,	1,	1,	'2022-11-03 10:00:00',	250,	50.00);
+insert into rental values(4,	1,	1,	'2022-11-04 10:00:00',	235,	50.00);
+insert into rental values(5,	1,	1,	'2022-12-05 10:00:00',	155,	50.00);
+insert into rental values(6,	2,	2,	'2022-12-08 11:00:00',	250,	60.00);
+insert into rental values(7,	3,	3,	'2022-12-13 12:00:00',	245,	40.00);
+insert into rental values(8,	1,	1,	'2023-01-05 10:00:00',	240,	50.00);
+insert into rental values(9,	2,	2,	'2023-01-08 11:00:00',	235,	60.00);
+insert into rental values(10,	3,	3,	'2023-02-13 12:00:00',	245,	40.00);
+insert into rental values(11,	1,	1,	'2023-03-05 10:00:00',	250,	50.00);
+insert into rental values(12,	2,	2,	'2023-03-08 11:00:00',	355,	60.00);
+insert into rental values(13,	3,	3,	'2023-04-13 12:00:00',	240,	40.00);
+insert into rental values(14,	1,	1,	'2023-04-01 10:00:00',	235,	50.00);
+insert into rental values(15,	1,	6,	'2023-05-01 10:00:00',	245,	60.00);
+insert into rental values(16,	1,	2,	'2023-05-01 10:00:00',	250,	60.00);
+insert into rental values(17,	1,	3,	'2023-06-01 10:00:00',	235,	40.00);
+insert into rental values(18,	1,	4,	'2023-06-01 10:00:00',	255,	75.00);
+insert into rental values(19,	1,	5,	'2023-07-01 10:00:00',	240,	50.00);
+insert into rental values(20,	2,	2,	'2023-07-02 11:00:00',	445,	60.00);
+insert into rental values(21,	3,	3,	'2023-07-03 12:00:00',	250,	40.00);
+insert into rental values(22,	4,	4,	'2023-08-04 13:00:00',	235,	75.00);
+insert into rental values(23,	5,	5,	'2023-08-05 14:00:00',	555,	50.00);
+insert into rental values(24,	6,	6,	'2023-09-06 15:00:00',	240,	60.00);
+insert into rental values(25,	7,	7,	'2023-09-07 16:00:00',	245,	40.00);
+insert into rental values(26,	8,	8,	'2023-09-08 17:00:00',	250,	75.00);
+insert into rental values(27,	9,	9,	'2023-10-09 18:00:00',	335,	50.00);
+insert into rental values(28,	10,	10,	'2023-10-10 19:00:00',	255,	60.00);
+insert into rental values(29,	10,	1,	'2023-10-10 19:00:00',	240,	50.00);
+insert into rental values(30,	10,	2,	'2023-10-10 19:00:00',	245,	60.00);
+insert into rental values(31,	10,	3,	'2023-10-10 19:00:00',	250,	40.00);
+insert into rental values(32,	10,	4,	'2023-10-10 19:00:00',	235,	75.00);
+
+
+--INSERT DATA INTO MEMBERSHIP_TYPE TABLE
+
+insert into membership_type values(1,'Basic Monthly',	'Unlimited rides with non-electric bikes. Renews monthly.',	100.00);
+insert into membership_type values(2,'Basic Annual',	'Unlimited rides with non-electric bikes. Renews annually.',500.00);
+insert into membership_type values(3,'Premium Monthly',	'Unlimited rides with all bikes. Renews monthly.',	200.00);
+
+
+--INSERT DATA INTO MEMBERSHIP TABLE
+
+insert into membership values(1,	2,	3,	'2023-08-01',	'2023-08-31',	500.00);
+insert into membership values(2,	1,	2,	'2023-08-01',	'2023-08-31',	100.00);
+insert into membership values(3,	3,	4,	'2023-08-01',	'2023-08-31',	200.00);
+insert into membership values(4,	1,	1,	'2023-09-01',	'2023-09-30',	100.00);
+insert into membership values(5,	2,	2,	'2023-09-01',	'2023-09-30',	500.00);
+insert into membership values(6,	3,	3,	'2023-09-01',	'2023-09-30',	200.00);
+insert into membership values(7,	1,	4,	'2023-10-01',	'2023-10-31',	100.00);
+insert into membership values(8,	2,	5,	'2023-10-01',	'2023-10-31',	500.00);
+insert into membership values(9,	3,	3,	'2023-10-01',	'2023-10-31',	200.00);
+insert into membership values(10,	3,	1,	'2023-11-01',	'2023-11-30',	200.00);
+insert into membership values(11,	2,	5,	'2023-11-01',	'2023-11-30',	500.00);
+insert into membership values(12,	1,	2,	'2023-11-01',	'2023-11-30',	100.00);
+
+select * from customer;
+select * from bike;
+select * from rental;
+select * from membership_type;
+select * from membership;
---
+++ Solution.sql
@@ -0,0 +1,188 @@
+USE BIKE_RENTAL_SHOP;
+
+--1. Emily would like to know how many bikes the shop owns by category
+--Display the category name and the number of bikes the shop owns in
+--each category (call this column number_of_bikes ). Show only the categories
+--where the number of bikes is greater than 2 .
+
+SELECT * FROM bike;
+
+SELECT category, COUNT(ID) AS number_of_bikes
+	FROM bike
+	GROUP BY category
+	HAVING COUNT(ID)>2;
+
+--2. Emily needs a list of customer names with the total number of
+--memberships purchased by each.
+--For each customer, display the customer's name and the count of
+--memberships purchased (call this column membership_count ). Sort the
+--results by membership_count , starting with the customer who has purchased
+--the highest number of memberships.
+
+
+SELECT customer.customer_name, COUNT(membership.ID) AS membership_count
+	FROM customer
+	RIGHT JOIN membership ON membership.customer_id = customer.ID
+	GROUP BY customer.customer_name
+	ORDER BY COUNT(membership.ID) DESC;
+
+
+--3. Emily is working on a special offer for the winter months. Can you help her
+--prepare a list of new rental prices?
+--For each bike, display its ID, category, old price per hour (call this column
+--old_price_per_hour ), discounted price per hour (call it new_price_per_hour ), old
+--price per day (call it old_price_per_day ), and discounted price per day (call it
+--new_price_per_day ).
+
+SELECT ID, category, price_per_hour AS old_price_per_hour,
+	ROUND(
+		CASE 
+			WHEN category = 'electric' THEN price_per_hour *.90
+			WHEN category = 'mountain bike' THEN price_per_hour *.80
+			ELSE price_per_hour * .50
+			END,2) AS new_price_per_hour,
+	price_per_day AS old_price_per_day,
+	ROUND(
+		CASE 
+			WHEN category = 'electric' THEN price_per_day*.80
+			WHEN category = 'mountain bike' THEN price_per_day*.50
+			ELSE price_per_day* .50
+			END,2) AS new_price_per_day
+	FROM bike;
+
+--4. Emily is looking for counts of the rented bikes and of the available bikes in
+--each category.
+--Display the number of available bikes (call this column
+--available_bikes_count ) and the number of rented bikes (call this column
+--rented_bikes_count ) by bike category.
+
+SELECT * FROM bike;
+
+SELECT category,
+		COUNT(CASE WHEN [status] = 'available' THEN 1 END) AS available_bike_count,
+		COUNT(CASE WHEN [status] = 'rented' THEN 1 END) AS rented_bike_count
+	FROM bike
+	GROUP BY category;
+
+--5. Emily is preparing a sales report. She needs to know the total revenue
+--from rentals by month, the total by year, and the all-time across all the
+--years. 
+--Display the total revenue from rentals for each month, the total for each
+--year, and the total across all the years. Do not take memberships into
+--account. There should be 3 columns: year , month , and revenue .
+--Sort the results chronologically. Display the year total after all the month
+--totals for the corresponding year. Show the all-time total as the last row.
+
+--SELECT * FROM rental;
+--SELECT YEAR(start_timestamp) AS year,
+--		MONTH(start_timestamp) AS month,
+--		SUM(total_paid)
+--	FROM rental
+--	GROUP BY YEAR(start_timestamp),MONTH(start_timestamp);
+
+--SELECT 
+--    YEAR(start_timestamp) AS year, 
+--    MONTH(start_timestamp) AS month, 
+--    SUM(total_paid) AS revenue
+--FROM rental
+--GROUP BY year(start_timestamp), month(start_timestamp)
+
+--UNION ALL
+
+SELECT 
+    YEAR(start_timestamp) AS year, 
+    NULL AS month, 
+    SUM(total_paid) AS revenue
+FROM rental
+GROUP BY year(start_timestamp)
+
+UNION ALL
+
+SELECT 
+    NULL AS year, 
+    NULL AS month, 
+    SUM(total_paid) AS revenue
+FROM rental
+
+UNION ALL 
+
+SELECT 
+    YEAR(start_timestamp) AS year, 
+    MONTH(start_timestamp) AS month, 
+    SUM(total_paid) AS revenue
+FROM rental
+GROUP BY year(start_timestamp), month(start_timestamp)
+
+ORDER BY year, month;
+
+
+--6. Emily has asked you to get the total revenue from memberships for each
+--combination of year, month, and membership type.
+--Display the year, the month, the name of the membership type (call this
+--column membership_type_name ), and the total revenue (call this column
+--total_revenue ) for every combination of year, month, and membership type.
+--Sort the results by year, month, and name of membership type.
+
+SELECT * FROM membership;
+
+SELECT YEAR(start_date) AS Year,
+		MONTH(start_date) AS Month,
+		membership_type.[name] AS membership_type_name,
+		SUM(total_paid) AS total_revenue
+		FROM membership
+		join membership_type ON membership.membership_type_id = membership_type.ID
+		GROUP BY YEAR(start_date),MONTH(start_date),membership_type.[name]
+		ORDER BY YEAR(start_date),MONTH(start_date),membership_type.[name];
+
+--7.Next, Emily would like data about memberships purchased in 2023, with
+--subtotals and grand totals for all the different combinations of membership
+--types and months.
+--Display the total revenue from memberships purchased in 2023 for each
+--combination of month and membership type. Generate subtotals and
+--grand totals for all possible combinations. There should be 3 columns:
+--membership_type_name , month , and total_revenue .
+
+
+SELECT membership_type.[name] AS membership_type_name,
+		MONTH(start_date) AS month,
+		SUM(total_paid) AS total_revenue
+		FROM membership JOIN membership_type ON membership.membership_type_id = membership_type.ID
+		WHERE YEAR(start_date) = 2023
+		GROUP BY CUBE(membership_type.name, MONTH(start_date))
+		ORDER BY membership_type.name, MONTH(start_date);
+
+
+--8. Emily wants to segment customers based on the number of rentals and
+--see the count of customers in each segment. Use your SQL skills to get
+--this!
+--Categorize customers based on their rental history as follows:
+--Customers who have had more than 10 rentals are categorized as 'more
+--than 10' .
+--Customers who have had 5 to 10 rentals (inclusive) are categorized as
+--'between 5 and 10' .
+--Customers who have had fewer than 5 rentals should be categorized as
+--'fewer than 5' .
+--Calculate the number of customers in each category. Display two columns:
+--rental_count_category (the rental count category) and customer_count (the
+--number of customers in each category).
+
+WITH cte AS 
+(
+    SELECT 
+        customer_id, 
+        COUNT(*) AS rental_count,
+        CASE 
+            WHEN COUNT(*) > 10 THEN 'more than 10' 
+            WHEN COUNT(*) BETWEEN 5 AND 10 THEN 'between 5 and 10'
+            ELSE 'fewer than 5'
+        END AS category
+    FROM rental
+    GROUP BY customer_id
+)
+SELECT 
+    category AS rental_count_category, 
+    COUNT(*) AS customer_count
+FROM cte 
+GROUP BY category
+ORDER BY customer_count;
+
